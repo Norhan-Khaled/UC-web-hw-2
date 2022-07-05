bdbea22c7b7d519b02a2f7ad51e89d160a69dbe9
Norhan-Khaled <107856437+Norhan-Khaled@users.noreply.github.com>
2022-07-06 01:41:17 +0300

    btn and icon not working

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+windwindow.location.hash = "Action";
+windwindow.location.hash = "Thriller";
+windwindow.location.hash = "Sci-Fi";
+windwindow.location.hash = "Horror";
+
+var bck2TopBtn = document.getElementById("topBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    bck2TopBtn.style.display = "block";
+  } else {
+    bck2TopBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function subscribeF() {
+  alert("هل تريد الاشتراك في منصتي 📺؟ (رد بـ نعم او لا");
+}
+
+var subscribe = prompt("هل تريد الاشتراك في منصتي 📺؟ (رد بـ نعم او لا");
+if (subscribe == "نعم") {
+  alert("مرحباً بك في منصتي 👋");
+} else if (subscribe == "لا") {
+  alert("حسناً، عد لنا اذا غيرت رايك ☹️");
+}
